c7fc3bdad328d1ef01251a8c7c28fe79ba151e93
Sol <noguerasol22@gmail.com>
2024-01-06 22:43:19 -0300

    Implement routes with react router dom. Add firebase to the project and custom hooks to use some functions. Create Screens: Landing, Register and Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Footer, Banner, Main, Navbar, BannerSection, SellingSection, SearchSection, HowToStartSection, AboutUsSection, LivestockPocketSection } from './components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Landing from './screens/Landing';
+import Login from './screens/Login';
+import Register from './screens/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <div className='bg-[#FBFAFA] relative min-h-[100dvh] flex w-full flex-col'>
-        <Banner />
-        <Navbar />
-        <div className='flex-grow flex justify-center mx-4 sm:mx-32 md:mx-64'>
-            <div className='flex flex-col gap-8 md:gap-16 lg:gap-20 '>
-              <BannerSection />
-              <SellingSection />
-              <SearchSection />
-              <HowToStartSection />
-              <AboutUsSection />
-              <img className="self-center" src="assets/logo.svg" alt="Logo company" width="104" height="56"></img>
-              <LivestockPocketSection />
-              <Footer />
-
-            </div>
-        </div>
-        {/* <Main /> */}
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/hooks/useFirebase.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, Auth } from "firebase/auth";
+
+const useFirebase = () => {
+    const [app, setApp] = useState<FirebaseApp>()
+    const [auth, setAuth] = useState<Auth | undefined>()
+    useEffect(()=>{
+        const firebaseConfig = {
+            apiKey: "AIzaSyDkZVTFbvbFYV-CBjdLNlv6JoKqBYiHS8o",
+            authDomain: "frontiersmarket-challenge.firebaseapp.com",
+            projectId: "frontiersmarket-challenge",
+            storageBucket: "frontiersmarket-challenge.appspot.com",
+            messagingSenderId: "884925322336",
+            appId: "1:884925322336:web:2673c4f678ed8c8efc5f05",
+            measurementId: "G-3W68RJ07PP"
+          };
+          
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        console.log("app", app)
+        setApp(app);
+        const analytics = getAnalytics(app);
+        console.log("analytics", analytics)
+        // Initialize Firebase Authentication and get a reference to the service
+        const auth = getAuth(app);
+        setAuth(auth)
+        auth.useDeviceLanguage();
+        console.log("auth", auth)
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/auth.user
+              const uid = user.uid;
+              alert(`New user signed in ${uid}`);
+              console.log(user)
+              // ...
+            } else {
+              // User is signed out
+            }
+          });
+    },[])
+  
+    // return { formData, handleChange, resetForm };
+    /**
+     * .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+     */ 
+    //currentUser.providerData
+    return { app, user: auth?.currentUser, createUserWithEmailAndPassword, signInWithEmailAndPassword }
+  };
+
+  export default useFirebase;
